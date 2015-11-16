c341c6da442761166c2bd2fc77b58a45b5f57189
Void <void@voidbookpro.local>
2015-11-16 07:56:55 +0100

    Initial Commit

---
+++ NativePath/NativePath.h
@@ -0,0 +1,13 @@
+//
+//  NativePath.h
+//  NativePath
+//
+//  Created by Void on 11/16/15.
+//  Copyright © 2015 Voidtarget. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NativePath : NSObject
+
+@end
---
+++ NativePath/NativePath.m
@@ -0,0 +1,13 @@
+//
+//  NativePath.m
+//  NativePath
+//
+//  Created by Void on 11/16/15.
+//  Copyright © 2015 Voidtarget. All rights reserved.
+//
+
+#import "NativePath.h"
+
+@implementation NativePath
+
+@end
