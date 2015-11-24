feee8ff1be9ea9fece616c2e8210c492f9a1cc96
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-11-24 12:51:17 +0900

    Added android scripts

---
+++ Android/jni/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := NativePath
+LOCAL_C_INCLUDES := \
+   $(LOCAL_PATH)/../../NativePath
+
+
+LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS := $(LOCAL_C_INCLUDES:%=-I%) -O3 -DANDROID_NDK
+
+LOCAL_SRC_FILES := \
+   ../../NativePath/NativeMath.c \
+   ../../NativePath/NativeMemory.c \
+   ../../NativePath/lol_trig.cpp 
+
+include $(BUILD_STATIC_LIBRARY)
