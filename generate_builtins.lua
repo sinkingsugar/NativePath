f11c35713e852dcbb064beea449a04911a20b3dc
Giovanni Petrantoni <giovanni.petrantoni@siliconstudio.co.jp>
2015-12-04 12:37:43 +0900

    added more Builtins

--- generate_builtins.lua
+++ generate_builtins.lua
@@ -1,7 +1,8 @@
 io.input("Builtins.def")
 io.output("output.txt")
 
-local define = "#if !__has_builtin(__builtin_x)\n#error \"x clang built-in not available\"\n#else\n#define x __builtin_x\n#endif"
+local define = "#if !__has_builtin(__builtin_x)\n\t#error \"x clang built-in not available\"\n#else\n\t#define x __builtin_x\n#endif\n"
+local define2 = "#if !__has_builtin(x)\n\t#error \"x clang built-in not available\"\n#endif\n"
 
 while true do
 	local line = io.read()
@@ -10,6 +11,13 @@ while true do
 	for w in string.gmatch(line, "BUILTIN%(__builtin_(.+)%s*,%s*\".+\"%s*,%s*\".+\"%)") do
 		w = string.gsub(w, " ", "");
 		local func = string.gsub(define, "x", w)
+		io.write(func)
+		io.write("\n")
+	end	
+	
+	for w in string.gmatch(line, "BUILTIN%((__sync_.+)%s*,%s*\".+\"%s*,%s*\".+\"%)") do
+		w = string.gsub(w, " ", "");
+		local func = string.gsub(define2, "x", w)
 		io.write(func)
 		io.write("\n")
 	end	
