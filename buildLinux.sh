3b51d792b72663ee267bdffcb889dacb6ad70dff
Manu <manu-silicon@users.noreply.github.com>
2016-04-28 14:09:18 +0900

    Build static libraries on Linux

--- buildLinux.sh
+++ buildLinux.sh
@@ -1,15 +1,24 @@
+#!/bin/sh
+
 mkdir -p build/Linux/x86
 mkdir -p build/Linux/x86_64
 
+sources="NativePath/NativeMath.c NativePath/NativeMemory.c NativePath/lol_trig.cpp NativePath/NativeTime.cpp NativePath/NativeDynamicLinking.c"
+
+if [ "$CC" = "" ]; then
+    CC=clang-3.6
+fi
+
+CFLAGS="-Wall -Wno-unused-const-variable -Wno-unused-function"
+
 #Linux 64
-gcc -c NativePath/NativeMath.c NativePath/NativeMemory.c NativePath/lol_trig.cpp NativePath/NativeTime.cpp NativePath/NativeDynamicLinking.c -fPIC -m64
-gcc -shared -Wl,-soname,libNativePath.so -o libNativePath.so *.o
-mv libNativePath.so build/Linux/x86_64/
+$CC -fPIC -m64 $CFLAGS -c $sources
+ar rcs libNativePath.a *.o
+mv libNativePath.a build/Linux/x86_64/
 rm *.o
 
 #Linux 32
-#gcc -c NativePath/NativeMath.c NativePath/NativeMemory.c NativePath/lol_trig.cpp NativePath/NativeTime.cpp -fPIC -m32
-#gcc -shared -Wl,-soname,libNativePath.so -o libNativePath.so *.o
-#mv libNativePath.so build/Linux/x86/
-#rm *.o
-
+$CC -m32 $CFLAGS -c $sources
+ar rcs libNativePath.a *.o
+mv libNativePath.a build/Linux/x86/
+rm *.o
