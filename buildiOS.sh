118e80e1c4cd36d9da8dfdd904b775399d7fee7e
Manu <manu-silicon@users.noreply.github.com>
2016-06-27 23:53:46 +0900

    Add MacOS library generation

--- buildiOS.sh
+++
@@ -1,5 +0,0 @@
-#xcodebuild clean
-xcodebuild VALID_ARCHS="armv7 armv7s arm64" ARCHS="armv7 armv7s arm64" ONLY_ACTIVE_ARCH=NO -sdk iphoneos -mios-version-min=6.0
-xcodebuild VALID_ARCHS="x86_64 i386" ARCHS="x86_64 i386" ONLY_ACTIVE_ARCH=NO -sdk iphonesimulator -mios-version-min=6.0
-lipo build/Release-iphoneos/libNativePath.a build/Release-iphonesimulator/libNativePath.a -create -output build/libNativePath.a
-file build/libNativePath.a
