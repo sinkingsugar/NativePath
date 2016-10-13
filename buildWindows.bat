00fd29c7fc17a7f3261af33ef0b5d794cbf9b54e
Manu <manu-silicon@users.noreply.github.com>
2016-10-13 10:48:25 -0700

    Only create directories if they don't already exist

--- buildWindows.bat
+++ buildWindows.bat
@@ -4,7 +4,7 @@ call "%PROGRAMFILES(X86)%\Microsoft Visual Studio 14.0\vc\vcvarsall.bat" x86
 set WindowsSdkDir=
 
 REM Windows
-mkdir build\Windows\x86
+if not exist build\Windows\x86 mkdir build\Windows\x86
 pushd build\Windows\x86
 cmake ..\..\..\ -G "Visual Studio 14"
 msbuild libNativePath.sln /p:Configuration=Release
@@ -12,7 +12,7 @@ if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
 REM Windows x64
-mkdir build\Windows\x64
+if not exist build\Windows\x64 mkdir build\Windows\x64
 pushd build\Windows\x64
 cmake ..\..\..\ -G "Visual Studio 14 Win64"
 msbuild libNativePath.sln /p:Configuration=Release
@@ -20,21 +20,21 @@ if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
 REM Windows Store
-mkdir build\WindowsStore\x86
+if not exist build\WindowsStore\x86 mkdir build\WindowsStore\x86
 pushd build\WindowsStore\x86
 cmake ..\..\..\ -G "Visual Studio 14" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1
 msbuild libNativePath.sln /p:Configuration=Release
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
-mkdir build\WindowsStore\x64
+if not exist build\WindowsStore\x64 mkdir build\WindowsStore\x64
 pushd build\WindowsStore\x64
 cmake ..\..\..\ -G "Visual Studio 14 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1
 msbuild libNativePath.sln /p:Configuration=Release
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
-mkdir build\WindowsStore\ARM
+if not exist build\WindowsStore\ARM mkdir build\WindowsStore\ARM
 pushd build\WindowsStore\ARM
 cmake ..\..\..\ -G "Visual Studio 14 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=8.1
 msbuild libNativePath.sln /p:Configuration=Release
@@ -42,14 +42,14 @@ if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
 REM Windows Phone
-mkdir build\WindowsPhone\x86
+if not exist build\WindowsPhone\x86 mkdir build\WindowsPhone\x86
 pushd build\WindowsPhone\x86
 cmake ..\..\..\ -G "Visual Studio 14" -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1
 msbuild libNativePath.sln /p:Configuration=Release
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
-mkdir build\WindowsPhone\ARM
+if not exist build\WindowsPhone\ARM mkdir build\WindowsPhone\ARM
 pushd build\WindowsPhone\ARM
 cmake ..\..\..\ -G "Visual Studio 14 ARM" -DCMAKE_SYSTEM_NAME=WindowsPhone -DCMAKE_SYSTEM_VERSION=8.1
 msbuild libNativePath.sln /p:Configuration=Release
@@ -57,21 +57,21 @@ if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
 REM Windows Universal
-mkdir build\WindowsUniversal\x86
+if not exist build\WindowsUniversal\x86 mkdir build\WindowsUniversal\x86
 pushd build\WindowsUniversal\x86
 cmake ..\..\..\ -G "Visual Studio 14" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0
 msbuild libNativePath.sln /p:Configuration=Release
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
-mkdir build\WindowsUniversal\x64
+if not exist build\WindowsUniversal\x64 mkdir build\WindowsUniversal\x64
 pushd build\WindowsUniversal\x64
 cmake ..\..\..\ -G "Visual Studio 14 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0
 msbuild libNativePath.sln /p:Configuration=Release
 if %ERRORLEVEL% neq 0 GOTO :error_popd
 popd
 
-mkdir build\WindowsUniversal\ARM
+if not exist build\WindowsUniversal\ARM mkdir build\WindowsUniversal\ARM
 pushd build\WindowsUniversal\ARM
 cmake ..\..\..\ -G "Visual Studio 14 ARM" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0
 msbuild libNativePath.sln /p:Configuration=Release
